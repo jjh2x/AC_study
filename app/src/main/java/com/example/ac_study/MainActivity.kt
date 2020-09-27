e716746221f2b3898a12ed49431496973b6f2c70
jjh2x <68693224+jjh2x@users.noreply.github.com>
2020-09-27 19:04:35 +0900

    test1

--- app/src/main/java/com/example/ac_study/MainActivity.kt
+++ app/src/main/java/com/example/ac_study/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.ac_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var text1: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
