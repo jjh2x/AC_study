5ea8885386b90176453f068dd45f00da409ce5ed
jjh2x <68693224+jjh2x@users.noreply.github.com>
2020-10-05 19:26:57 +0900

    버튼 누르면 현재 시간 띄우기(모듈화하여 코딩)

--- app/src/main/java/com/example/ac_study/MainActivity.kt
+++ app/src/main/java/com/example/ac_study/MainActivity.kt
@@ -3,12 +3,33 @@ package com.example.ac_study
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var text1: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTimeButton()
+
     }
+
+    private fun setTimeButton() {
+        time_button.setOnClickListener {
+            showMessage(getCurrentTime())
+        }
+    }
+
+    private fun getCurrentTime(): String {
+        return Calendar.getInstance().getTime().toString()
+
+    }
+
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT /* 보여주는 시간*/).show()
+    } //인자에 해당하는 토스트 메시지(String형) 출력.
 }
