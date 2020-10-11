8d805dd1cf697c03f0c3c4bb37cc5ee69e49ad5d
jjh2x <68693224+jjh2x@users.noreply.github.com>
2020-10-11 10:22:24 +0900

    앱센터 과제2 : 버튼을 누르면 버튼이 사라지고 1초 후에 다시 나타나게 하는 방법. 키워드 : View, visibility, Handler().postDelayed({},time)

--- app/src/main/java/com/example/ac_study/MainActivity.kt
+++ app/src/main/java/com/example/ac_study/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ac_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,17 +21,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun setTimeButton() {
         time_button.setOnClickListener {
-            showMessage(getCurrentTime())
+            time_button.visibility=View.INVISIBLE
+            Handler().postDelayed({
+                time_button.visibility = View.VISIBLE
+            }, 1000)
         }
     }
 
+
     private fun getCurrentTime(): String {
         return Calendar.getInstance().getTime().toString()
-
     }
-
 
     private fun showMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT /* 보여주는 시간*/).show()
     } //인자에 해당하는 토스트 메시지(String형) 출력.
+
 }
